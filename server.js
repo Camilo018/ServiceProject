c4a1e0ca515437c31102772c75ebf7407893771c
Camilo López <50026257+Camilo018@users.noreply.github.com>
2020-08-05 21:25:03 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-// server.js
-'use strict';
-const http = require('http');
-const server = http.createServer(function (req, res) {
-    res.writeHead(200, {'content-type': 'text/plain'});
-    res.end('Hola Mundo');
+var express = require('express');
+var app = express();
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+  let numbers = [between(10, 300), between(1, 5), between(10, 300), between(1000, 1200), between(5, 15)]
+  console.log('El tamaño de la lista es: ' + numbers.length);
+  numbers.forEach(element => {
+  console.log('El numero ' + element + ' es ' + element.toString(2) + ' en binario');
+  });
 });
-server.listen(8000);
+
+function between(min, max) {  
+  return Math.floor(
+    Math.random() * (max - min) + min
+  )
+}
+
+app.listen(3200, function () {
+  console.log('La aplicación esta corriendo en el puerto ' + this.address().port);
+});
